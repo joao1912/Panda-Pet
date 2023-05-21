fa1ab666563e7dc1e0ba3edbf5bbc2e66c452915
joão <joaopedrot1912@gmail.com>
2023-05-21 20:46:00 -0300

    Função de erro atualizada

--- scripts/utils/erros.js
+++ scripts/utils/erros.js
@@ -3,6 +3,8 @@ export function Error(erro) {
     let containerIcones
     let icon
 
+    //erro no cadastro
+
     switch(erro) {
         case "#userName-vazio#":
             containerErro = document.getElementById("erroCadastroUserName")
@@ -61,5 +63,47 @@ export function Error(erro) {
             icon = document.getElementById("iconVisibilityConfirm")
             icon.style.top = "60%"
             break
+    }
+
+    //erro no login
+    const iconePassword = document.getElementById("iconLoginVisibility")
+    switch(erro) {
+        case "#userName-vazio#":
+            containerErro = document.getElementById("erroLoginConfirm")
+            containerErro.innerHTML = `Este campo é obrigatório!`
+            containerErro.style.marginLeft = "-190px"
+
+            iconePassword.style.top = "58.5%"
+        break
+
+        case "#passWord-vazio#":
+            containerErro = document.getElementById("erroLoginPassWord")
+            containerErro.innerHTML = `Este campo é obrigatório!`
+            containerErro.style.marginLeft = "-190px"
+
+
+
+            break
+
+        case "#senha-invalida#":
+            containerErro = document.getElementById("erroLoginPassWord")
+            containerErro.innerHTML = `A senha deve ter no mínimo 6 caracteres e 3 numeros!`
+            containerErro.style.marginLeft = "-16px"
+
+
+            break
+        
+        case "#usuario-inexistente#":
+            containerErro = document.getElementById("erroLoginConfirm")
+            containerErro.innerHTML = `Este usuário não existe!`
+            containerErro.style.marginLeft = "-205px"
+
+            break
+
+        case "#senha-incorreta#":
+            containerErro = document.getElementById("erroLoginPassWord")
+            containerErro.innerHTML = `A senha está incorreta!`
+            containerErro.style.marginLeft = "-215px"
+
     }
 }
