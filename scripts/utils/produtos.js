3880420c29aa31e7fe411019f77ff3778657d545
joão <joaopedrot1912@gmail.com>
2023-05-20 22:52:40 -0300

    Criação de um array de objetos com produtos de testes

---
+++ scripts/utils/produtos.js
@@ -0,0 +1,64 @@
+function Produto(nome, codigo, preco, img) {
+    this.nome = nome
+    this.codigo = codigo
+    this.preco = preco
+    this.img = img
+}
+
+const produtos = [
+
+    new Produto("Produto de teste 1", 0, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 1, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 2, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 3, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 4, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 5, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 6, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 7, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 8, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 9, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 10, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 11, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 12, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 13, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 14, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 15, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 16, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 17, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 18, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 19, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 20, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 21, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 22, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 23, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 24, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 25, 150, '../imagens/foto 300x300.png'),
+
+    new Produto("Produto de teste 1", 26, 150, '../imagens/foto 300x300.png')
+
+]
