961582c6ecbfcbcc0514199e29af64c3999139a1
João Pedro T <joaopedrot1912@gmail.com>
2023-06-12 15:45:29 -0300

    trocando style para atributo
    - agora quando for escurecer um td você ira ser usado um atributo
    
    - função de marcar foi refatorada

--- scripts/pagina-adm/script.js
+++ scripts/pagina-adm/script.js
@@ -271,7 +271,7 @@ function setCurrentDay() {
                 } 
             }
         }
-        
+
     } else {
 
         for(let dayIterable of weeks) {
@@ -518,82 +518,82 @@ function setDarkCellCalendar() {
     let week5 = [...bodyCalendar[4].children]
 
     for (let i = 0 ; i < 7 ; i++) {
-        week1[i].classList = ""
-        week5[i].classList = ""
+        week1[i].removeAttribute("escurecer")
+        week5[i].removeAttribute("escurecer")
     }
-    console.log("setDarkCalendar: " + mesAtual)
+    
     switch(mesAtual) {
         case 0:
-            week1[0].classList += "escurecer"
+            week1[0].setAttribute("escurecer", true)
             for (let i = 4; i < 7 ; i++) {
-                week5[i].classList += "escurecer"
+                week5[i].setAttribute("escurecer", true)
             }
             break
         case 1:
             for (let i = 0 ; i < 3 ; i++) {
-                week1[i].classList += "escurecer"
+                week1[i].setAttribute("escurecer", true)
             }
             for (let i = 3; i < 7 ; i++) {
-                week5[i].classList += "escurecer"
+                week5[i].setAttribute("escurecer", true)
             }
             break
         case 2:
             for (let i = 0 ; i < 3 ; i++) {
-                week1[i].classList += "escurecer"
+                week1[i].setAttribute("escurecer", true)
             }
-            week5[6].classList += "escurecer"
+            week5[6].setAttribute("escurecer", true)
             break
         case 3:
             for (let i = 0 ; i < 6 ; i++) {
-                week1[i].classList += "escurecer"
+                week1[i].setAttribute("escurecer", true)
             }
             break
         case 4:
-            week1[0].classList += "escurecer"
+            week1[0].setAttribute("escurecer", true)
 
             for(let i = 4 ; i < 7 ; i++) {
-                week5[i].classList += "escurecer"
+                week5[i].setAttribute("escurecer", true)
             }
             break
         case 5:
             for(let i = 0 ; i < 4 ; i++) {
-                week1[i].classList += "escurecer"
+                week1[i].setAttribute("escurecer", true)
             }
-            week5[6].classList += "escurecer"
+            week5[6].setAttribute("escurecer", true)
             break
         case 6:
             for (let i = 0 ; i < 6 ; i++) {
-                week1[i].classList += "escurecer"
+                week1[i].setAttribute("escurecer", true)
             }
             break
         case 7:
-            week1[0].classList += "escurecer"
-            week1[1].classList += "escurecer"
+            week1[0].setAttribute("escurecer", true)
+            week1[1].setAttribute("escurecer", true)
 
-            week5[5].classList += "escurecer"
-            week5[6].classList += "escurecer"
+            week5[5].setAttribute("escurecer", true)
+            week5[6].setAttribute("escurecer", true)
             break
         case 8: 
             for (let i = 0 ; i < 5 ; i++) {
-                week1[i].classList += "escurecer"
+                week1[i].setAttribute("escurecer", true)
             }
             break
         case 9:
             for (let i = 3 ; i < 7 ; i++) {
-                week5[i].classList += "escurecer"
+                week5[i].setAttribute("escurecer", true)
             }
             break
         case 10:
             for (let i = 0 ; i < 3 ; i++) {
-                week1[i].classList += "escurecer"
+                week1[i].setAttribute("escurecer", true)
             }
 
-            week5[5].classList += "escurecer"
-            week5[6].classList += "escurecer"
+            week5[5].setAttribute("escurecer", true)
+            week5[6].setAttribute("escurecer", true)
             break
         case 11:
             for (let i = 0 ; i < 5 ; i++) {
-                week1[i].classList += "escurecer"
+                week1[i].setAttribute("escurecer", true)
             }
             
             break
@@ -623,8 +623,8 @@ agendamentos.push(
     {
         id: userID,
         service: "Hospedagem",
-        dia: 30,
-        mes: 10 //o mes vai de 0 a 11 !!!
+        dia: 29,
+        mes: 9 //o mes vai de 0 a 11 !!!
     }
 )
 
@@ -656,55 +656,39 @@ function setMarkersCalendar() {
                 
                 if (day.textContent == agendamento.dia) {
                     let verifyClass = false
-                    ;[...day.classList].forEach(classe => {
-                        if (classe == "mark") {
-                            verifyClass = true
-                        }
 
-                        if (classe == "escurecer") {
-                            verifyClass = true
-                            
-                            for (let agendamentoEscuro of agendamentos) {
-                                console.log(`mes na agenda: ${agendamentoEscuro.mes} | mes atual: ${mesAtual - 1}`)
-                                if (agendamentoEscuro.mes == (mesAtual - 1)) {
-                                    
-                                    for (let day of weeks) {
-                                        
-                                        if (day.textContent == agendamentoEscuro.dia && day.classList.contains("escurecer")) {
-                                            day.classList += " mark"
-                                        }
-                                    }
-                                }
-                            }
+                    if (day.classList.contains("mark")) {
+                        verifyClass = true
+                    }
 
-                        }
-                    })
+                    if (day.getAttribute("escurecer")) {
+                        
+                        verifyClass = true
+                        
 
+                    }
+                    
                     if (!verifyClass) {
                         day.classList += "mark"
                     }
 
                 }
-
-                ;[...day.classList].forEach(classe => {
-                    if (classe == "escurecer") {
-                    
+                
+                if (day.getAttribute("escurecer")) {
+                
                     for (let agendamentoEscuro of agendamentos) {
-                        console.log(`mes na agenda: ${agendamentoEscuro.mes} | mes atual: ${mesAtual - 1}`)
+                        
                         if (agendamentoEscuro.mes == (mesAtual - 1)) {
                             
                             for (let day of weeks) {
                                 
-                                if (day.textContent == agendamentoEscuro.dia && day.classList.contains("escurecer")) {
+                                if (day.textContent == agendamentoEscuro.dia && day.getAttribute("escurecer")) {
                                     day.classList += " mark"
                                 }
                             }
                         }
                     }
-
                 }
-                }) 
-
             }
         }
     }
