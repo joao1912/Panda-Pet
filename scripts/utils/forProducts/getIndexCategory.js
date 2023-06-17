fdcd4dfd9e796754aaf63764df3a00854348c905
DalisonCPU <dalisonmg@gmail.com>
2023-06-17 19:51:03 -0300

    Feita função para mostrar produtos aleatórios na página de produtos

--- scripts/utils/forProducts/getIndexCategory.js
+++ scripts/utils/forProducts/getIndexCategory.js
@@ -2,15 +2,38 @@ import { produtos } from "../produtos.js"
 
 export function getIndexCategory(getCategoria) {
     let indexProd = []
-    let index = 0
-    for(let obj of produtos) {
-        
-        if (obj.categoria === getCategoria) {
-            indexProd.push(index)
-        }
-    
-        index++
-    }
+    if (getCategoria == "sugestoes") {
+        let quantityProducts = produtos.length
 
+        if (quantityProducts > 12) {
+            quantityProducts = 12
+        }
+
+        while (indexProd.length < 12) {
+
+            var randomNumber = Math.floor(Math.random() * produtos.length)
+
+            if (indexProd.indexOf(randomNumber) > -1) {
+
+                continue
+
+            }
+
+            indexProd.push(randomNumber)
+        }
+
+    } else {
+        let index = 0
+        for (let obj of produtos) {
+
+            if (obj.categoria === getCategoria) {
+                indexProd.push(index)
+            }
+
+            index++
+        }
+
+    }
+    
     return indexProd
 }
