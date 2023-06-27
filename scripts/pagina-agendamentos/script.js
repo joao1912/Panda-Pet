9fafa6b1e3b2569a6062de2eca286c7c819cbd52
Zuma Elma Pereira <zumapereira@hotmail.com>
2023-06-27 17:47:47 -0300

    alteracoes no formulario do panda pet

--- scripts/pagina-agendamentos/script.js
+++ scripts/pagina-agendamentos/script.js
@@ -201,3 +201,29 @@ function barraPesquisa() {
         }
     })
 }
+
+const botaoEscolha = document.getElementById("botaoEscolha")
+const btnYes = document.getElementById("btnYes")
+let btnBefore 
+
+;[...document.styleSheets[2].cssRules].forEach( styleSheet => {
+  if (styleSheet.selectorText == "#btnYes::before") {
+    btnBefore = styleSheet
+  }
+  
+})
+
+botaoEscolha.addEventListener("click", function(event){
+    let id = event.target.id
+
+    switch(id) {
+        case "btnYes":
+            btnBefore.style.left = "3px"
+            btnYes.dataset.content = "Sim"
+            break
+        case "btnNo":
+            btnBefore.style.left = "102px"
+            btnYes.dataset.content = "Não"
+            break
+    }
+})
