a302ca719bfdeffb4e4d228316a7a2cf228de1b8
joão <joaopedrot1912@gmail.com>
2023-06-27 09:40:56 -0300

    Atualização no StyleGuide
    - Botão de Escolha consertado
    - Logo do Panda Pet Atualizado
    - Icones Adicionados

--- style-guide/script.js
+++ style-guide/script.js
@@ -1,16 +1,25 @@
-const containerBotaoEscolha = document.getElementById("botaoEscolha")
-const botaoDeTroca = document.getElementById("botaoTrocaEscolha")
-containerBotaoEscolha.addEventListener("click", function(event){
-    const id = event.target.id
+const botaoEscolha = document.getElementById("botaoEscolha")
+const btnYes = document.getElementById("btnYes")
+let btnBefore 
 
-    if (id === "op2") {
-        botaoDeTroca.style.left = "68.2%"
-    } else if (id === "op1") {
-        botaoDeTroca.style.left = "62%"
-    }
- 
+;[...document.styleSheets[2].cssRules].forEach( styleSheet => {
+  if (styleSheet.selectorText == "#btnYes::before") {
+    btnBefore = styleSheet
+  }
+  
 })
 
-function teste() {
-    window.scroll(0,findPos(document.getElementById("containerIcones")));
-}
+botaoEscolha.addEventListener("click", function(event){
+    let id = event.target.id
+
+    switch(id) {
+        case "btnYes":
+            btnBefore.style.left = "3px"
+            btnYes.dataset.content = "Sim"
+            break
+        case "btnNo":
+            btnBefore.style.left = "102px"
+            btnYes.dataset.content = "Não"
+            break
+    }
+})
