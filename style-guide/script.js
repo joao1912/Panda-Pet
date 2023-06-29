c5db5b69dece01847c88f3ff5c85638b4575af85
joão <joaopedrot1912@gmail.com>
2023-06-29 09:32:24 -0300

    libs adicionadas no style guide

--- style-guide/script.js
+++ style-guide/script.js
@@ -2,7 +2,7 @@ const botaoEscolha = document.getElementById("botaoEscolha")
 const btnYes = document.getElementById("btnYes")
 let btnBefore 
 
-;[...document.styleSheets[2].cssRules].forEach( styleSheet => {
+;[...document.styleSheets[4].cssRules].forEach( styleSheet => {
   if (styleSheet.selectorText == "#btnYes::before") {
     btnBefore = styleSheet
   }
@@ -22,4 +22,52 @@ botaoEscolha.addEventListener("click", function(event){
             btnYes.dataset.content = "Não"
             break
     }
+})
+
+
+const botaoAlert = document.getElementById("btnAlert")
+
+botaoAlert.addEventListener("click", function(){
+  Swal.fire(
+    'Olha esse alerta!',
+    'Você clicou no botão!',
+    'success'
+  )
+})
+
+$('#carrossel').slick({
+  dots: true,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2500
+});
+
+var ctx = document.getElementById("chart")
+
+var chartGraph = new Chart(ctx, {
+  type: "pie",
+  data: {
+      labels: [
+          'exemplo1',
+          'exemplo2',
+          'exemplo3',
+          'exemplo4',
+          'exemplo5'
+      ],
+      datasets: [{
+          label: 'Rendimento Bruto',
+          data: [300,100,100,100,50],
+          backgroundColor: [
+              'rgb(255, 99, 132)',
+              'rgb(54, 162, 235)',
+              'rgb(255, 205, 86)',
+              'rgb(217, 94, 50)',
+              'rgb(255, 3, 33)'
+          ],
+          hoverOffset: 4
+      }]
+  }
 })
