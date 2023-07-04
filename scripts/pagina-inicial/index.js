e0133234a8e0b7d4725d9d3045cd852f4d945cbe
Danieli Bordulis <danieli_b_silva@estudante.sesisenai.org.br>
2023-07-04 17:44:02 -0300

    Botão de fechar e script da pagina
    fiz o JS pra pegar os dados do cliente para os inputs

--- scripts/pagina-inicial/index.js
+++ scripts/pagina-inicial/index.js
@@ -239,15 +239,46 @@ $('#container').slick({
     autoplaySpeed: 2500
   });
 
-  $('#container2').slick({
-    dots: true,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1000
-  });
+  let nome 
+  let dataConta
+  let img
+
+function puxarInfos(){
+   
+    let users=localStorage.getItem("users")
+    users=JSON.parse(users) 
+
+    for(let obj of users){
+       
+
+        if(obj.id == userID){
+            nome=obj.nome
+            dataConta=obj.date.text
+            img=obj.img
+        
+        }
+    }
+   inserirInfo()
+}
+
+puxarInfos()
+
+function inserirInfo(){
+   let nomeUser = document.getElementById("nomeUser")
+   let contaUser= document.getElementById("contaUser")
+   let imagem=document.getElementById("imagem")
+
+   nomeUser.value=nome
+   contaUser.value=dataConta
+   imagem.src=img
+
+}
+
+
+
+
+
+ 
 
 
   
