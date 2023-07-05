d1e91ad6cf9316d5fc6c8107c045d823d6c6eebc
Zuma Elma Pereira <zumapereira@hotmail.com>
2023-07-05 17:41:35 -0300

    terminei css pagina 3 falta j.s

---
+++ scripts/utils/getIndexCategory.js
@@ -0,0 +1,16 @@
+import { produtos } from "./produtos.js"
+
+export function getIndexCategory(getCategoria) {
+    let indexProd = []
+    let index = 0
+    for(let obj of produtos) {
+        
+        if (obj.categoria === getCategoria) {
+            indexProd.push(index)
+        }
+    
+        index++
+    }
+
+    return indexProd
+}
