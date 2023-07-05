1828e14b6822f3f49645f80b4b8faae7089624d7
Danieli Bordulis <danieli_b_silva@estudante.sesisenai.org.br>
2023-07-05 17:49:04 -0300

    adicionei imagens
    adicionei todos os produtos

--- scripts/utils/produtos.js
+++ scripts/utils/produtos.js
@@ -24,37 +24,37 @@ function initializeProducts() {
     const initialProducts = [
         //Categoria: Alimentação
 
-        new Produto(0, "Bifinho sabor carne", "Bifinho saudável para seu cachorro sabor carne.", "../../imagens/alimentacao/bifinhoSaborCarne.png", 29,90, "alimentacao", "", 10),
-        new Produto(1, "Gran Nature", "Ração natural com ingredientes orgânicos para gatos.", "../../imagens/alimentacao/gran-nature.png", 35,99, "alimentacao", "", 10),
-        new Produto(2, "Petisco Dental para Cães", "Petisco saboroso que auxilia na saúde bucal dos cães.", "petisco1.jpg", 12.99, "alimentacao", "", 10),
-        new Produto(3, "Purina Pro Plan Focus", "Ração com fórmula especializada para fornecer nutrição de qualidade, ajudando a manter a saúde e o peso ideal dos cães adultos.", "racao3.jpg", 59.99, "alimentacao", "", 10),
-        new Produto(4, "Petisco Orgânico para Gatos", "Petisco orgânico e livre de conservantes para gatos exigentes.", "petisco2.jpg", 14.99, "alimentacao", "", 10),
-        new Produto(5, "Royal Canin Maxi Junior", "Ração especial para o crescimento saudável de filhotes de cães.", "racao4.jpg", 39.99, "alimentacao", "", 10),
-        new Produto(6, "Petisco Vegetariano para Cães", "Petisco vegetariano e saudável para cães.", "petisco3.jpg", 8.99, "alimentacao", "", 10),
-        new Produto(7, "Ração para Gatos Sênior", "Ração formulada para gatos idosos com necessidades especiais.", "racao5.jpg", 34.99, "alimentacao", "", 10),
-        new Produto(8, "Golden Formula Frango e Arroz", "Ração com ingredientes selecionados, como frango e arroz, oferecendo uma nutrição completa e equilibrada para cães adultos.", "petisco4.jpg", 11.99, "alimentacao", "", 10),
+        new Produto(0, "Bifinho sabor carne", "Bifinho saudável para seu cachorro, sabor carne.", "../../imagens/alimentacao/bifinhoSaborCarne.png", 19.90, "alimentacao", "", 10),
+        new Produto(1, "Ração para cachorro porte M", "Ração natural com ingredientes orgânicos para cachorros porte médio.", "../../imagens/alimentacao/gran-nature.png", 120.00, "alimentacao", "", 10),
+        new Produto(2, "Sache para gatos", "Petisco saboroso que auxilia na saúde do seu gatinho.", "../../imagens/alimentacao/mik-cat-sache.png", 12.99, "alimentacao", "", 10),
+        new Produto(3, "Sache para cachorros", "Petisco saboroso que auxilia na saúde do seu cachorro.", "../../imagens/alimentacao/mik-dog-sache-carne-filhote.png", 13.90, "alimentacao", "", 10),
+        new Produto(4, "Ossinhos", "Ossinhos orgânicos e livre de conservantes para cães.", "../../imagens/alimentacao/ossinhoDog.jpg", 14.99, "alimentacao", "", 10),
+        new Produto(5, "Peticos para gatos", "Peticos saudavéis sem conservantes.", "../../imagens/alimentacao/peticosCat.png", 19.99, "alimentacao", "", 10),
+        new Produto(6, "Ração para gatos", "Ração para seu gatinho que auxilia na sua saúde.", "../../imagens/alimentacao/racaoCat.png", 89.90, "alimentacao", "", 10),
+        new Produto(7, "Ração para gatos fihotes", "Ração formulada para gatinhos que acabaram de sair do leite.", "../../imagens/alimentacao/racaoFilhoteCat.jpg", 79.99, "alimentacao", "", 10),
+        new Produto(8, "Ração para cachorro porte grande", "Ração com ingredientes selecionados, como frango e arroz, oferecendo uma nutrição completa e equilibrada para cães adultos.", "../../imagens/alimentacao/soft-dog-caes.png", 59.99, "alimentacao", "", 10),
 
         // Categoria: Brinquedos
-        new Produto(9, "Bola para Cães", "Bola resistente e durável para cães se divertirem.", "bola1.jpg", 14.99, "brinquedos", "", 10),
-        new Produto(10, "Corda para Cães", "Corda resistente para cães brincarem e exercitarem a mandíbula.", "corda1.jpg", 9.99, "brinquedos", "", 10),
-        new Produto(11, "Arranhador para Gatos", "Arranhador com poste e plataforma para gatos afiarem as unhas.", "arranhador1.jpg", 29.99, "brinquedos", "", 10),
-        new Produto(12, "Bola de Pelúcia para Gatos", "Bola de pelúcia macia e colorida para gatos se divertirem.", "bola2.jpg", 6.99, "brinquedos", "", 10),
-        new Produto(13, "Osso de Borracha para Cães", "Osso de borracha resistente para cães brincarem e aliviarem o estresse.", "osso1.jpg", 12.99, "brinquedos", "", 10),
-        new Produto(14, "Labirinto Interativo para Gatos", "Labirinto com brinquedos e obstáculos para gatos se entreterem.", "labirinto1.jpg", 39.99, "brinquedos", "", 10),
-        new Produto(15, "Bola com Guizo para Gatos", "Bola com guizo para gatos perseguirem e se divertirem.", "bola3.jpg", 4.99, "brinquedos", "", 10),
-        new Produto(16, "Pelúcia com Apito para Cães", "Pelúcia fofa com apito para cães se divertirem e estimularem o instinto de caça.", "pelucia1.jpg", 8.99, "brinquedos", "", 10),
-        new Produto(17, "Bolinha de Tênis para Cães", "Bolinha de tênis durável para cães brincarem.", "bolinha1.jpg", 3.99, "brinquedos", "", 10),
+        new Produto(9, "Arranhador para Gatos", "Arranhador resistente com plataforma para gatos afiarem as unhas.", "../../imagens/brinquedos/arranhador-brinquedo-cat.jpg", 59.99, "brinquedos", "", 10),
+        new Produto(10, "Bolinhas de borracha", "Bolinhas para cães brincarem e exercitarem a mandíbula.", "../../imagens/brinquedos/bolinhasCaes.png", 9.99, "brinquedos", "", 10),
+        new Produto(11, "Arranhador com bolinhas para gatos", "Arranhador com poste e plataforma para gatos.", "../../imagens/brinquedos/brinquedo-c-arranhador-gatos.jpg", 45.90, "brinquedos", "", 10),
+        new Produto(12, "Brinquedo com penas", "penas coloridas para gatos se divertirem.", "../../imagens/brinquedos/brinquedo-gatos-penas.png", 29.99, "brinquedos", "", 10),
+        new Produto(13, "Ratinhos", "Ratinho interativo e resistente para seus pets brincarem e aliviarem o estresse.", "../../imagens/brinquedos/brinquedo-gatos-ratinho.png", 12.99, "brinquedos", "", 10),
+        new Produto(14, "Hamburguer para morder", "Hamburguinho de borracha para seu cachorro morder sem medo!.", "../../imagens/brinquedos/hamburguer-dog.png", 12.90, "brinquedos", "", 10),
+        new Produto(15, "Limpa dentes para cachorros", "O brinquedo ideal para seu pet se divertir e limpar seus dentinhos.", "../../imagens/brinquedos/limpadente.png", 22.90, "brinquedos", "", 10),
+        new Produto(16, "Mordedor para cães", "Mordedor ideal para seu pet se distrair.", "../../imagens/brinquedos/morderdorcaes.jpg", 35.99, "brinquedos", "", 10),
+        new Produto(17, "Osso de borracha", "Brinquedo durável para cães brincarem.", "../../imagens/brinquedos/osso-dog.jpg", 13.99, "brinquedos", "", 10),
 
         // Categoria: Acessórios
-        new Produto(18, "Coleira ajustável de nylon", "Coleira durável e ajustável para passeios com cachorros.", "coleira1.jpg", 19.99, "acessorios", "", 10),
-        new Produto(19, "Guia retrátil", "Guia de corda retrátil para oferecer mais liberdade de movimento durante os passeios.", "guia1.jpg", 29.99, "acessorios", "", 10),
-        new Produto(20, "Caixa de areia", "Caixa de areia higiênica para gatos fazerem suas necessidades de forma adequada.", "caixa_areia1.jpg", 24.99, "acessorios", "", 10),
-        new Produto(21, "Cama confortável", "Cama macia e aconchegante para proporcionar um lugar de descanso ao cachorro.", "cama1.jpg", 39.99, "acessorios", "", 10),
-        new Produto(22, "Comedouro automático", "Comedouro programável para fornecer alimento automaticamente em horários determinados.", "", 49.99, "acessorios", "", 10),
-        new Produto(23, "Cama aquecida", "Cama macia e aquecida para proporcionar conforto extra ao gato durante o sono.", "cama2.jpg", 62.99, "acessorios", "", 10),
-        new Produto(24, "Bebedouro automático", "Bebedouro com sistema de fluxo constante de água fresca para manter seu pet hidratado.", "bebedouro1.jpg", 45.99, "acessorios", "", 10),
-        new Produto(25, "Escova de pelos", "Escova de pelos com cerdas macias para cuidar da pelagem do seu pet.", "escova1.jpg", 9.99, "acessorios", "", 10),
-        new Produto(26, "Transportadora para Gatos", "Transportadora segura e confortável para transportar seu gato com facilidade.", "transportadora1.jpg", 49.99, "acessorios", "", 10)
+        new Produto(18, "Casinha com arranhador para gatos", "Casinha e arranhador para seu gatinho brincar", "../../imagens/acessorios/arranhadorCasaCat.jpg", 139.90, "acessorios", "", 10),
+        new Produto(19, "Cama toca para pets", "Caminha estilo toca para deixar seu pet confortável.", "../../imagens/acessorios/cama-toca.png", 79.90, "acessorios", "", 10),
+        new Produto(20, "Comedouro para pets", "Acessório incrivel para quando seus pets ficarem sozinhos, serve ocmo comedouro e bebedouro.", "../../imagens/acessorios/comedouro-bebedouro-automatico.jpg", 44.90, "acessorios", "", 10),
+        new Produto(21, "Comedouro para gatos", "Comedouro estilo cat.", "../../imagens/acessorios/comedouro-gatos.jpg", 22.99, "acessorios", "", 10),
+        new Produto(22, "Comedouro interativo", "Comedouro para seu pet desestressar.", "../../imagens/acessorios/comedouro-interativo.png", 39.99, "acessorios", "", 10),
+        new Produto(23, "Guia retrátil", "Guia retrátil para deixar seu pet confortável na hora do passeio.", "../../imagens/acessorios/guia-retratil.png", 25.99, "acessorios", "", 10),
+        new Produto(24, "Porta ração", "Mantenha a ração dos seus pets organizada, escolha o tamanho ideal para guardar a ração de maneira prática.", "../../imagens/acessorios/porta-racao-pet.jpg", 45.99, "acessorios", "", 10),
+        new Produto(25, "Casa tunel para gatos", "Casinha confortavél para seu gatinho.", "../../imagens/acessorios/toca-tunel-gato.jpg", 99.90, "acessorios", "", 10),
+        new Produto(26, "Cama pets", "Caminha ideal para seu pet, com o conforto que ele merece.", "imagens/acessorios/caminha-pet.png", 59.90, "acessorios", "", 10)
     ]
 
     localStorage.setItem("listaProdutos", JSON.stringify(initialProducts))
