f542138e81ae659337c8d2b7a4eb41bde2fa44ce
joão <joaopedrot1912@gmail.com>
2023-07-09 20:26:14 -0300

    tabela das 'minhas compras' feita

--- scripts/pag-inicial.js
+++ scripts/pag-inicial.js
@@ -1,3 +1,7 @@
+import {verifyUserOnline} from "./utils/verifyUserOnline.js"
+
+let users = JSON.parse(localStorage.getItem("users"))
+
 $('#containerCarrossel').slick({
     dots: true,
     infinite: true,
@@ -7,6 +11,8 @@ $('#containerCarrossel').slick({
     autoplay: true,
     autoplaySpeed: 2500
 });
+
+let userID = verifyUserOnline()
 
 const btnAgendar = document.getElementById("botaoTransparente")
 btnAgendar.addEventListener("click", function() {
@@ -23,6 +29,94 @@ btnAgendar.addEventListener("click", function() {
     })
 
     containerTelaAgendamento.style.display = "block"
+})
+
+const containerBtnsPerfilUser = document.getElementById("excluirEeditar")
+containerBtnsPerfilUser.addEventListener("click", function(event){
+    let id = event.target.id
+    const telaMinhasCompras = document.getElementById("containerMinhasCompras")
+
+    switch(id) {
+        case "btnExcluirUser":
+
+            break
+        case "btnEditUser":
+            
+            break
+        case "btnMyShopping":
+            setTableMyShopping()
+            telaMinhasCompras.style.display = "flex"
+
+            break
+    }
+})
+
+function setTableMyShopping() {
+    
+    let totCompras
+    let produtos = JSON.parse(localStorage.getItem("listaProdutos"))
+    const tbody = document.querySelector("#containerTable > table > tbody")
+    tbody.innerHTML = ""
+    for (let obj of users) {
+        if (obj.id === userID) {
+            totCompras = obj.atividadeNoSite.produtosComprados
+            break
+        }
+    }
+
+    for (let i = 0 ; i < totCompras.length ; i++) {
+
+        let nomeProd, codigoProd, precoProd
+        
+        for (let prod of produtos) {
+            if (prod.codigo == totCompras[i]) {
+                nomeProd = prod.nome
+                codigoProd = prod.codigo
+                precoProd = prod.preco
+                break
+            }
+        }
+
+        let tr = constructorTableMyShopping(codigoProd, nomeProd, precoProd)
+        tbody.appendChild(tr)
+    }
+}
+
+function constructorTableMyShopping(cod, nome, preco) {
+
+    let tdCodigo = document.createElement("td")
+    let tdNome = document.createElement("td")
+    let tdPreco = document.createElement("td")
+
+    let textCodigo = document.createTextNode(`${cod}`)
+    let textNome = document.createTextNode(`${nome}`)
+    let textPreco = document.createTextNode(`R$ ${preco.toFixed(2)}`)
+
+    tdCodigo.appendChild(textCodigo)
+    tdCodigo.classList.add("tdCodProduct")
+
+    tdNome.appendChild(textNome)
+
+    tdPreco.appendChild(textPreco)
+    tdPreco.classList.add("tdPrecoProduct")
+
+    let tr = document.createElement("tr")
+    tr.appendChild(tdCodigo)
+    tr.appendChild(tdNome)
+    tr.appendChild(tdPreco)
+
+    return tr
+}
+
+const botaoFecharMinhasCompras = document.getElementById("btnCloseMinhasCompras")
+botaoFecharMinhasCompras.addEventListener("click", function(){
+    const telaMinhasCompras = document.getElementById("containerMinhasCompras")
+    
+    if (telaMinhasCompras.style.display === "none") {
+        telaMinhasCompras.style.display = "flex"
+    } else {
+        telaMinhasCompras.style.display = "none"
+    }
 })
 
 let nome 
