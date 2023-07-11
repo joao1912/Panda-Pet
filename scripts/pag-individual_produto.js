ab16540e91e00d1c0c7f21c657aafd84d0bb8f18
joão <joaopedrot1912@gmail.com>
2023-07-11 10:27:38 -0300

    botao de adicionar e remover do carrnho na pag-individual-produto esta funcionando

--- scripts/pag-individual_produto.js
+++ scripts/pag-individual_produto.js
@@ -1,10 +1,29 @@
+import {verifyUserOnline} from "./utils/verifyUserOnline.js"
+import {saveLocalStorage} from "./utils/saveLocalStorage.js"
 let nome,descricao,imagem,descricaoImagem,preco,classificacao
+
+const btnCarrinho = document.getElementById("btnCarrinhoProdIndividual")
+const userID = verifyUserOnline()
+
+let carrinho
 
 
 export function loadInfoProducts(codigoProduto) {
 
     let listaProdutos = JSON.parse(localStorage.getItem("listaProdutos")) 
+    let users = JSON.parse(localStorage.getItem("users"))
 
+    if (userID == undefined) {
+        window.location.href = "../paginas/cadastro-login.html"
+    }
+
+    for (let user of users) {
+        if (user.id == userID) {
+            carrinho = user.carrinho
+    
+        }
+    }
+    
     for (let obj of listaProdutos){
 
         if (obj.codigo == codigoProduto){
@@ -36,6 +55,24 @@ export function loadInfoProducts(codigoProduto) {
     imgProduto.alt = descricaoImagem
 
     setStars(classificacao)
+
+    btnCarrinho.textContent = "Adicionar no Carrinho"
+
+    if (carrinho.length > 0) {
+        for (let prod of carrinho) {
+            if (prod.codigo == codigoProduto) {
+                btnCarrinho.textContent = "Remover do Carrinho"
+            }
+        }
+    }
+
+    if (btnCarrinho.textContent == "Remover do Carrinho") {
+        btnCarrinho.addEventListener("click", () => {removerItemCarrinho(codigoProduto)})
+    } else if ("Adicionar no Carrinho") {
+        btnCarrinho.addEventListener("click", () => {addItemCarrinho(codigoProduto)})
+    }
+
+
 }
 
 
@@ -65,3 +102,45 @@ function setStars(classificacao) {
     }
 }
 
+function removerItemCarrinho(cod) {
+    let users = JSON.parse(localStorage.getItem("users"))
+
+    for (let i = 0 ; i < carrinho.length ; i++) {
+        if (carrinho[i].codigo == cod) {
+            carrinho.splice(i, 1)
+            break
+        }
+    }
+
+    for (let user of users) {
+        if (user.id == userID) {
+            user.carrinho = carrinho
+            
+        }
+    }
+    saveLocalStorage(users)
+
+    btnCarrinho.textContent = "Adicionar no Carrinho"
+    
+}
+
+function addItemCarrinho(cod) {
+    let users = JSON.parse(localStorage.getItem("users"))
+
+    let produtoAdd = {
+        codigo: Number(cod),
+        quantidade: 1
+    }
+
+    for (let user of users) {
+        if (user.id == userID) {
+            user.carrinho.push(produtoAdd)
+            
+        }
+    }
+
+    saveLocalStorage(users)
+
+    btnCarrinho.textContent = "Remover do Carrinho"
+
+}
