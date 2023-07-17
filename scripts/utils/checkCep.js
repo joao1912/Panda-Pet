0deafd0438d3ee679fc7c076574db6986bcce67d
Dalison <dalisonmg@gmail.com>
2023-07-17 15:16:14 -0300

    Testando mais uma vez

--- scripts/utils/checkCep.js
+++ scripts/utils/checkCep.js
@@ -1,36 +1,61 @@
 let obj_cep
 
 function enviar() {
-    const input = document.getElementById("CEP").value
-    validaCep(input)
+   const input = document.getElementById("CEP").value
+   validaCep(input)
 }
 
 export async function validaCep(cep) {
 
-    await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    .then(response => response.json())
-    .then(json => obj_cep = json)
+   await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(response => response.json())
+      .then(json => obj_cep = json)
 
-     if(obj_cep.code) {
-        Swal.fire({icon: 'error',
-        title: 'Oops...',
-        text: `${obj_cep.message}`})
-     } else {
-        //salvar o cep e calcular o frete
-     }
-    
+   if (obj_cep.code) {
+      Swal.fire({
+         icon: 'error',
+         title: 'Oops...',
+         text: `${obj_cep.message}`
+      })
+   } else {
+      //salvar o cep e calcular o frete
+   }
+
 
 }
 
 export async function calcularFrete(cep) {
-   try {
-   await fetch(`https://www.cepcerto.com/ws/json-frete/88032005/${cep.value}/1000`)
-   .then(response => response.json())
-       .then(json => obj_cep = json) 
-         console.log(obj_cep)
-         alert("encontrado")
-   } catch {
-      alert("erro ao carregar cep")
-      return null
-   }
+   // try {
+   // await fetch(`https://www.cepcerto.com/ws/json-frete/88032005/${cep.value}/1000`)
+   // .then(response => response.json())
+   //     .then(json => obj_cep = json) 
+   //       console.log(obj_cep)
+   //       alert("encontrado")
+   // } catch {
+   //    alert("erro ao carregar cep")
+   //    return null
+   // }
+
+
+
+
+   let headers = new Headers();
+
+   headers.append('Content-Type', 'application/json');
+   headers.append('Accept', 'application/json');
+
+   headers.append('Access-Control-Allow-Origin', 'https://joao1912.github.io/Panda-Pet');
+   headers.append('GET', 'POST', 'OPTIONS');
+
+
+   fetch(`https://www.cepcerto.com/ws/json-frete/88032005/${cep.value}/1000`, {
+      mode: 'no-cors',
+      method: 'GET',
+      headers: headers
+   })
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.log('Authorization failed : ' + error.message));
+
+
 }
